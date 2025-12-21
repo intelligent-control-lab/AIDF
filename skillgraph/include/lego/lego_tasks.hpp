b15cfca2db3993b4dc3381401a189e3c5c4217a2
Philip Huang <philip.huang@outlook.com>
2025-12-21 15:19:36 -0500

    chore(skillgraph): standardize license headers

--- skillgraph/include/lego/lego_tasks.hpp
+++ skillgraph/include/lego/lego_tasks.hpp
@@ -1,3 +1,32 @@
+/*
+**********************************************************************************************************************
+Ontology and Skill Graph for Autonomous Multi-Robot Assembly
+AI Data Foundry (AIDF) Project
+
+Copyright (c) 2025
+Carnegie Mellon University
+ARM Institute – Advanced Robotics for Manufacturing
+
+Authors:
+    Philip Huang philiphuang@cmu.edu 
+    Peiqi Yu peiqiy@andrew.cmu.edu 
+    Chaitanya Chawla cchawla@cs.cmu.edu 
+    Changliu Liu cliu6@andrew.cmu.edu 
+    Jiaoyang Li jiaoyanl@andrew.cmu.edu 
+    Guanya Shi guanyas@andrew.cmu.edu
+
+Non-Commercial Research License:
+Permission is hereby granted to use, copy, modify, and distribute this Software for non-commercial research and
+educational purposes only, provided that the above copyright notice and this permission notice appear in all
+copies or substantial portions of the Software.
+
+Commercial use of this Software, in whole or in part, requires explicit written permission from Carnegie Mellon
+University and the ARM Institute.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
+**********************************************************************************************************************
+*/
+
 #pragma once
 #include "tasks.hpp"
 #include "lego/Lego.hpp"
